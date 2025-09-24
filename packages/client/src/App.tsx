6fc33b7bdbc5248e34c927c5e8c06d7cdc5c1e0d
Yi Miao <ppp_melody@163.com>
2025-09-24 15:38:36 +0800

    feat: build chatBox frontend and backend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from 'react';
+import Chatbot from './components/chat/Chatbot';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
   return (
-    <>
-      <p>{message}</p>
-    </>
+    <div className="p-4">
+      <Chatbot />
+    </div>
   );
 }
 
