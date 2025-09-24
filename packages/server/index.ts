8e1f411e0263ac56e926ed9233814952dd707950
Yi Miao <ppp_melody@163.com>
2025-09-24 15:17:10 +0800

    feat: add base code and connect frontend with backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello via Bun!');
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
